a7f9ce711db0ca437bae067a0b4e5ca5bfc96783
THCSC <tobias@skancode.dk>
2024-03-01 23:21:53 +0000

    added mutations

--- server/src/graphql/schema.ts
+++ server/src/graphql/schema.ts
@@ -2,6 +2,7 @@ import { League } from "../model/league";
 import { Club } from "../model/club";
 import { IShirt, Shirt } from "../model/shirt"
 import { ObjectId } from "mongodb";
+import { Bid, IBid } from "../model/bid";
 
 export const typeDefs = `#graphql
 	type League {
@@ -16,6 +17,33 @@ export const typeDefs = `#graphql
 		name: String!,
 		league: League!,
 		shirts: [Shirt],
+		createdAt: String,
+	}
+
+	input CreateShirtInput {
+		title: String!,
+		description: String!,
+		condition: String!,
+		year: String!,
+		clubId: ID!,
+		sellerId: ID!,
+		playerName: String,
+		playerNumber: Int,
+		price: Int!,
+		minPrice: Int!
+	}
+
+	input UpdateShirtInput {
+		title: String!,
+		description: String!,
+		condition: String!,
+		year: String!,
+		clubId: ID!,
+		sellerId: ID!,
+		playerName: String,
+		playerNumber: Int,
+		price: Int!,
+		minPrice: Int!,
 		createdAt: String,
 	}
 
@@ -46,6 +74,22 @@ export const typeDefs = `#graphql
 		createdAt: String,
 	}
 
+	input CreateBidInput {
+		ownerId: ID!,
+		shirtId: ID!,
+		amount: Int,
+		expiryDate: String
+	}
+
+	input UpdateBidInput {
+		owner: ID!,
+		shirt: ID!,
+		amount: Int,
+		expiryDate: String,
+		accepted: Boolean,
+		declined: Boolean,
+	}
+
 	type Bid {
 		_id: ID!,
 		owner: User!,
@@ -68,14 +112,14 @@ export const typeDefs = `#graphql
 		shirtsByUserId(userId: ID): [Shirt],
 	}
 
-	type Mutations {
-		createShirt(title: String!, description: String!, condition: String!, year: String!, club: Club!, seller: User!, playerName: String, playerNumber: Int, price: Int!, minPrice: Int!): Shirt,
-		updateShirt(updateShirt: Shirt!): Shirt,
+	type Mutation {
+		createShirt(input: CreateShirtInput): Shirt,
+		updateShirt(shirtId: ID!, input: UpdateShirtInput): Shirt,
 		deleteShirtById(id: ID!): Shirt,
-		acceptBid(bidId: ID!, userId: ID!): Bid,
-		declineBid(bidId: ID!, userId: ID!): Bid,
-		createBid(ownerId: ID!, shirtId: Id!, amount: Int, expiryDate: String): Bid,
-		updateBid(updatedBid: Bid!): Bid,
+		acceptBid(bidId: ID!): Bid,
+		declineBid(bidId: ID!): Bid,
+		createBid(input: CreateBidInput): Bid,
+		updateBid(bidId: ID!, input: UpdateBidInput): Bid,
 		deleteBidById(bidId: ID!): Bid,
 	}
 `;
@@ -90,7 +134,7 @@ export const resolvers = {
 			console.log("Start finding clubs");
 			return Club.find().then((data) => data);
 		},
-		allClubsByLeague: async (_, {leagueId}) => {
+		allClubsByLeague: async (_: never, {leagueId}) => {
 			console.log("Start finding clubs by league")
 			return Club.find({"league._id": leagueId});
 		},
@@ -101,7 +145,7 @@ export const resolvers = {
 				.populate('bids')
 				.then((data) => data);
 		},
-		shirtsByLeague: async (_, {leagueId}: {leagueId: string}) => {
+		shirtsByLeague: async (_: never, {leagueId}: {leagueId: string}) => {
 			const league = await League
 				.findOne({_id: new ObjectId(leagueId)})
 				.populate({
@@ -126,7 +170,7 @@ export const resolvers = {
 
 			return shirts;
 		},
-		shirtsByClub: async (_, {clubId}: {clubId: string}) => {
+		shirtsByClub: async (_: never, {clubId}: {clubId: string}) => {
 			const club = await Club.findOne({_id: new ObjectId(clubId)});
 			if (!club) {
 				return [];
@@ -138,11 +182,121 @@ export const resolvers = {
 
 			return club.shirts;
 		},
-		shirtById: async (_, {shirtId}) => {
+		shirtById: async (_: never, {shirtId}) => {
 			return Shirt.findById(shirtId);
 		},
-		shirtsByUserId: async (_, {userId}) => {
+		shirtsByUserId: async (_: never, {userId}) => {
 			return Shirt.find({"user._id": userId});
 		}
+	},
+	Mutation: {
+		createShirt: async (_: never, {input}: CreateShirtArgs) => {
+			const newShirt = await Shirt.create({
+				_id: new ObjectId(),
+				club: new ObjectId(input.clubId),
+				seller: new ObjectId(input.sellerId),
+				...input
+			});
+
+			await newShirt.populate('club');
+			await newShirt.populate('seller');
+
+			return newShirt;
+		},
+		updateShirt: async (_: never, {shirtId, input}: UpdateShirtArgs) => {
+			await Shirt.updateOne({_id: shirtId}, {...input})
+			return await Shirt.findById(shirtId)
+				.populate('club')
+				.populate('seller');
+		},
+		deleteShirtById: async (_: never, {shirtId}: {shirtId: string}) => {
+			return await Shirt.findByIdAndDelete(shirtId);
+		},
+		acceptBid: async (_: never, {bidId}: {bidId: string}) => {
+			await Bid.updateOne({_id: bidId}, {accepted: true});
+			return await Bid.findById(bidId);
+		},
+		declineBid: async (_: never, {bidId}: {bidId: string}) => {
+			await Bid.updateOne({_id: bidId}, {declined: true});
+			return await Bid.findById(bidId);
+		},
+		createBid: async (_: never, {input}: CreateBidArgs) => {
+			const newBid = await Bid.create({
+				_id: new ObjectId(),
+				owner: new ObjectId(input.ownerId),
+				shirt: new ObjectId(input.shirtId),
+				expiryDate: new Date(input.expiryDate),
+				amount: input.amount,
+			});
+
+			await newBid.populate('owner');
+			await newBid.populate('shirt');
+
+			return newBid;
+		},
+		updateBid: async (_: never, {bidId, input}: UpdateBidArgs) => {
+			await Bid.updateOne({_id: bidId}, {...input})
+			return await Bid.findById(bidId)
+				.populate('shirt')
+				.populate('owner');
+		},
+		deleteBidById: async (_: never, {bidId}: {bidId: string}) => {
+			return Bid.findByIdAndDelete(bidId);
+		},
 	}
 };
+
+interface CreateShirtArgs {
+	input: {
+		title: string,
+		description: string,
+		condition: string,
+		year: string,
+		clubId: string,
+		sellerId: string,
+		playerName: string,
+		playerNumber: number,
+		price: number,
+		minPrice: number
+	}
+}
+
+interface UpdateShirtArgs {
+	shirtId: string,
+	input: {
+		title: string,
+		description: string,
+		condition: string,
+		year: string,
+		club: string,
+		seller: string,
+		playerName: string,
+		playerNumber: number,
+		bids: [string],
+		activeBids: [string],
+		price: number,
+		minPrice: number,
+		createdAt: string,
+	}
+}
+
+interface CreateBidArgs {
+	input: {
+		ownerId: string,
+		shirtId: string,
+		amount: number,
+		expiryDate: string
+	}
+}
+
+interface UpdateBidArgs {
+	bidId: string, 
+	input: {
+		owner: string,
+		shirt: string,
+		amount: number,
+		expiryDate: string,
+		accepted: boolean,
+		declined: boolean,
+	}
+}
