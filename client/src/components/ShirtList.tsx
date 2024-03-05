d322d47bc8411fc0fbca6021bf2bda846b299bef
THCSC <tobias@skancode.dk>
2024-03-05 13:25:45 +0100

    remove default export, and formatting

--- client/src/components/ShirtList.tsx
+++ client/src/components/ShirtList.tsx
@@ -1,74 +1,69 @@
 import { useQuery } from "@apollo/client";
 import { QueryGetShirts, gqlGetShirts } from "../graphql/shirt"; 
+import { CSSProperties } from "react";
 
-const ShirtList = () => {
+export const ShirtList = () => {
 
-    const h1Styles = {
-        textAlign: 'center'
-    }
-    
-    const { data, loading } = useQuery<QueryGetShirts>(gqlGetShirts);
-    
-    if(loading) {
-        return (
-            <h1 style={h1Styles}>Loading Shirts</h1>
-            );
-        }
-        
-        return ( 
-            <>
-            <h1 style={h1Styles}>Shirt List</h1>
-            <ShirtListRenderer allShirts={data?.allShirts!}/>
+  const h1Styles: CSSProperties = {
+    textAlign: "center"
+  }
 
-        </>
-     );
-    }
-    
-    
-    function ShirtListRenderer({ allShirts: shirts }: QueryGetShirts) {
-  
-        const containerStyles = {
-          fontWeight: '10px',
-        };
-      
-        const paragraphStyles = {
-          fontWeight: 'bold',
-        };
-      
-        const spanStyles = {
-          fontWeight: 'normal',
-        };
-      
-        return (
-          <div style={containerStyles}>
-            {shirts.map((shirt) => (
-              <div key={shirt._id}>
-                <hr />
-                <h3>
-                  Player: <span style={spanStyles}>{shirt.playerName}</span>
-                </h3>
-                <p style={paragraphStyles}>
-                  Number: <span style={spanStyles}>#{shirt.playerNumber}</span>
-                </p>
-                <p style={paragraphStyles}>
-                  Price: <span style={spanStyles}>{shirt.price}</span>
-                </p>
-                <p style={paragraphStyles}>
-                  Details: <span style={spanStyles}>{shirt.description}</span>
-                </p>
-                <p style={paragraphStyles}>
-                  Condition: <span style={spanStyles}>{shirt.condition}</span>
-                </p>
-                <p style={paragraphStyles}>
-                  Seller: <span style={spanStyles}>{shirt.seller?.username}</span>
-                </p>
-              </div>
-            ))}
-          </div>
-        );
-      }
-      
-      
+  const { data, loading } = useQuery<QueryGetShirts>(gqlGetShirts);
 
- 
-export default ShirtList;
+  if(loading) {
+    return (
+      <h1 style={h1Styles}>Loading Shirts</h1>
+    );
+  }
+
+  return ( 
+    <>
+      <h1 style={h1Styles}>Shirt List</h1>
+      <ShirtListRenderer allShirts={data?.allShirts!}/>
+    </>
+  );
+}
+
+
+function ShirtListRenderer({ allShirts: shirts }: QueryGetShirts) {
+
+  const containerStyles = {
+    fontWeight: '10px',
+  };
+
+  const paragraphStyles = {
+    fontWeight: 'bold',
+  };
+
+  const spanStyles = {
+    fontWeight: 'normal',
+  };
+
+  return (
+    <div style={containerStyles}>
+      {shirts.map((shirt) => (
+        <div key={shirt._id}>
+          <hr />
+          <h3>
+            Player: <span style={spanStyles}>{shirt.playerName}</span>
+          </h3>
+          <p style={paragraphStyles}>
+            Number: <span style={spanStyles}>#{shirt.playerNumber}</span>
+          </p>
+          <p style={paragraphStyles}>
+            Price: <span style={spanStyles}>{shirt.price}</span>
+          </p>
+          <p style={paragraphStyles}>
+            Details: <span style={spanStyles}>{shirt.description}</span>
+          </p>
+          <p style={paragraphStyles}>
+            Condition: <span style={spanStyles}>{shirt.condition}</span>
+          </p>
+          <p style={paragraphStyles}>
+            Seller: <span style={spanStyles}>{shirt.seller?.username}</span>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
