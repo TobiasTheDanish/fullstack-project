e68baf91a352d23f8ae883f85614b68c87ca84d2
THCSC <tobias@skancode.dk>
2024-03-05 13:22:48 +0100

    move router setup from Navbar to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ShirtList, LeagueList, ClubList, Navbar } from './components';
+import App from './App';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -12,7 +14,16 @@ const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={ <App /> }>
+            <Route path="/leagues" element={<LeagueList />} />
+            <Route path="/clubs" element={<ClubList />} />
+            <Route path="/all-shirts" element={<ShirtList />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
 )
