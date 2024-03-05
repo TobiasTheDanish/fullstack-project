bb7f6e2b5746f941bcfb69351082bbe24763ec0e
THCSC <tobias@skancode.dk>
2024-03-05 13:21:36 +0100

    make app primary route, with outlet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar'
+import './App.css';
+import { Outlet, useLocation } from 'react-router-dom'
 
 function App() {
+  const location = useLocation()
+  if (location.pathname === '/') {
+    return (
+      <>
+        <h1>Hello, and welcome to our page</h1>
+      </>
+    );
+  }
   return (
     <>
-      <Navbar/>
+      <Outlet />
     </>
   )
 }
