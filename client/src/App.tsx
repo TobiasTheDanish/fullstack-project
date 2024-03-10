0282d2a3b904e84165b3c08aaf44cfd1128e82ef
THCSC <tobias@skancode.dk>
2024-03-10 16:56:30 +0100

    succesfully uploads one image at a time

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,34 @@
+import { FormEvent, useState } from 'react';
 import './App.css';
 import { Outlet, useLocation } from 'react-router-dom'
 
 function App() {
+  const [image, setImage] = useState<string>("");
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.nativeEvent.target as HTMLFormElement);
+    const res = await fetch("http://localhost:4000/upload/single", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (res.ok) {
+      const body:{message: string, path:string} = await res.json()
+      setImage(body.path);
+    }
+  }
+
   const location = useLocation()
   if (location.pathname === '/') {
     return (
       <>
         <h1>Hello, and welcome to our page</h1>
+        <form encType='multipart/form-data' onSubmit={submitHandler}>
+          <input type='file' name='file' />
+          <input type='submit' value={"Submit"}/>
+        </form>
+        {image != "" && <img src={image} />}
       </>
     );
   }
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,11 +6,17 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './graphql/schema'
 import mongoose from 'mongoose';
 import { GraphQLContext } from './graphql/utils';
+import multer from 'multer';
+import { Client } from 'file-storing';
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 dotenv.config();
 
 async function main() {
 	const app = express()
+
+	app.use(cors());
 
 	mongoose.connect(process.env.MONGODB_CONN_STR!.replace('<password>', process.env.MONGODB_PW!));
 
@@ -27,6 +33,36 @@ async function main() {
 			jwtSecret: process.env.JWT_SECRET!,
 		};
 	}}));
+
+	app.post('/upload/single', upload.single('file'), async (req, res, next) => {
+		if (!req.file) {
+			res.status(400).json({
+				error: "No file provided.",
+			});
+			return next();
+		}
+		const { buffer } = req.file;
+
+		const storageClient = new Client({
+			region: "eu-central-1",
+			credentials: {
+				accessKeyId: process.env.S3_ACCESS!,
+				secretAccessKey: process.env.S3_SECRET!,
+			},
+		});
+		const uploadRes = await storageClient.upload({
+			key: crypto.randomUUID(),
+			body: buffer,
+			bucket: "football-shirts-fullstack-proj",
+			objectAccess: "public-read",
+		}, (_p) => {})
+
+		res.status(200).json({
+			message: "Upload succesfull",
+			/*@ts-ignore*/
+			path: uploadRes.Location,
+		});
+	});
 
 	app.listen(4000);
 	console.log("Listening on localhost:4000");
