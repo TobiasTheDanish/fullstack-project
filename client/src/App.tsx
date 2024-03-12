1bfe6fa7693d418209c33eae9a258d82c66d43c5
THCSC <tobias@skancode.dk>
2024-03-12 12:00:36 +0100

    add start to CreateShirt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react';
 import './App.css';
 import { Outlet, useLocation } from 'react-router-dom'
-import { ImageUpload } from './components/ImageUpload';
 
 function App() {
-  const [image, setImage] = useState<string>("");
 
   const location = useLocation()
   if (location.pathname === '/') {
     return (
       <>
         <h1>Hello, and welcome to our page</h1>
-        <ImageUpload setImage={setImage} />
-        {image != "" && <img src={image} />}
       </>
     );
   }
--- client/src/components/ImageUpload.tsx
+++ client/src/components/ImageUpload.tsx
@@ -1,13 +1,17 @@
-import { Dispatch, FormEvent, SetStateAction } from 'react';
+import { ChangeEvent, Dispatch, SetStateAction } from 'react';
 
 interface Props {
   setImage: Dispatch<SetStateAction<string>>
 }
 
 export function ImageUpload({setImage}: Props ) {
-  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+  const submitHandler = async (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.nativeEvent.target as HTMLFormElement);
+    if (!e.target.files) return;
+
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.set("file", file);
     const res = await fetch("http://localhost:4000/upload/single", {
       method: "POST",
       body: formData,
@@ -16,13 +20,11 @@ export function ImageUpload({setImage}: Props ) {
     if (res.ok) {
       const body:{message: string, path:string} = await res.json()
       setImage(body.path);
+      e.target.value = "";
     }
   }
 
   return (
-    <form encType='multipart/form-data' onSubmit={submitHandler}>
-      <input type='file' name='file' />
-      <input type='submit' value={"Submit"}/>
-    </form>
+      <input type='file' name='file' onChange={submitHandler}/>
   );
 }
