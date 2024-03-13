733df4aa87da7ac766bbec7d05e5166fd39296d5
THCSC <tobias@skancode.dk>
2024-03-13 11:30:30 +0100

    conditional rendering based on login status

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,8 +1,16 @@
 import './Navbar.css'
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import { CircleUserRound, Home } from 'lucide-react';
+import { authManager } from '../../lib/utils';
+import { useEffect, useState } from 'react';
 
 export const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | undefined>(undefined);
+  const location = useLocation();
+
+  useEffect(() => {
+    setIsLoggedIn(authManager.isLoggedIn());
+  }, [location.pathname]);
   return (
     <div className='navbarContainer'>
       <div className='iconContainer'>
@@ -21,11 +29,23 @@ export const Navbar = () => {
           All Shirts
         </NavLink>
       </div>
+      {isLoggedIn ? (
       <div className='iconContainer'>
         <Link to="/profile" className="linkIcon">
           <CircleUserRound color="#ffffff" width="32" height="32" />
         </Link>
       </div>
+      ) : (
+        <div className='signInContainer'>
+          <NavLink to="/sign-in" className="link">
+            Sign In
+          </NavLink>
+          <NavLink to="/sign-up" className="link">
+            Sign up
+          </NavLink>
+        </div>
+      )
+      }
     </div>
   );
 };
