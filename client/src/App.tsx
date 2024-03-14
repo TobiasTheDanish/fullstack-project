4341ad9132408e9783d8499a988bea3312acebd4
ChristianStennicke <cph-cs519@cphbusiness.dk>
2024-03-14 11:35:43 +0100

    Tailwind implementeret + button effekt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { QueryGetShirts, gqlGetShirts } from './graphql/shirt';
 import { Shirt } from './graphql/types';
 import { useEffect, useState } from 'react';
 import RecentlyAddedShirts from './components/shirts/RecentlyAddedShirts';
+import netherlandsHomeKitImage from './assets/imgs/Netherlands-Home-Kit-1988.jpg';
 
 function App() {
   const location = useLocation();
@@ -12,45 +13,66 @@ function App() {
 
   const { data } = useQuery<QueryGetShirts>(gqlGetShirts);
 
-  useEffect (() =>  {
-    if(!data) {
+  useEffect(() => {
+    if (!data) {
       return
     }
 
-    const sortShirts: Shirt[] = [...data.allShirts].sort((a: Shirt, b: Shirt)=> {
+    const sortShirts: Shirt[] = [...data.allShirts].sort((a: Shirt, b: Shirt) => {
       return parseInt(b.createdAt!) - parseInt(a.createdAt!)
     })
     const recentlyAddedShirts: Shirt[] = sortShirts.slice(0, 4) || [];
     setShirts(recentlyAddedShirts)
   }, [data])
 
+
+
   if (location.pathname === '/') {
     return (
       <>
         <div id="root">
-          <div className="background-section">
-            <div className="background-content">
-              <h1>Jersey Hub</h1>
-            </div>
-          </div>
-          <div className="content-container">
-            <div className="homepage">
-              <h2>Find your unique football shirts here</h2>
-              <Link to="/all-shirts">
-                <div className='clickable-box'>
-                  Discover
-                </div>
-              </Link>
+          <div className="hero bg-cover bg-center h-screen flex items-center justify-center text-white" style={{ backgroundImage: `url(${netherlandsHomeKitImage})`, 
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center', 
+          height: '120vh' }}>
+            <h1 className="text-4xl md:text-6xl font-bold mb-6">Jersey Hub</h1>
+            <div className="text-center hero-content" style={{ paddingTop: '60vh' }}>
+              <h2 className="text-2xl md:text-4xl font-semibold mb-10">Find your unique football shirts</h2>
+              <div>
+                <Link to={"/all-shirts"}>
+                  <button
+                    style={{
+                      backgroundColor: '#007bff',
+                      color: '#fff',
+                      padding: '10px 20px',
+                      borderRadius: '5px',
+                      transition: 'background-color 0.3s, color 0.3s',
+                    }}
+                    onMouseEnter={(e: React.MouseEvent<HTMLButtonElement>) => {
+                      e.currentTarget.style.backgroundColor = '#fff';
+                      e.currentTarget.style.color = '#007bff';
+                    }}
+                    onMouseLeave={(e: React.MouseEvent<HTMLButtonElement>) => {
+                      e.currentTarget.style.backgroundColor = '#007bff';
+                      e.currentTarget.style.color = '#fff';
+                    }}
+                  >
+                    Discover
+                  </button>
+
+                </Link>
+              </div>
+
             </div>
           </div>
           <div className="recently-added">
             <h2>Recently Added</h2>
-            <RecentlyAddedShirts shirts={shirts}/>
+            <RecentlyAddedShirts shirts={shirts} />
           </div>
         </div>
       </>
     );
-  } 
+  }
   return (
     <>
       <div id="root">
