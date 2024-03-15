2461249a55449ce24b7dac41466932dd2f4bdccb
ChristianStennicke <cph-cs519@cphbusiness.dk>
2024-03-15 16:16:06 +0100

    implementeret tailwind - mangler baggrund på sm skærm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,50 +27,24 @@ function App() {
 
   if (location.pathname === '/') {
     return (
-      <>
-        <div id="root">
-          <div className="hero bg-cover bg-center sm:bg-left" 
-          style={{ backgroundImage: `url(${netherlandsHomeKitImage})`, 
-          backgroundSize: 'cover', 
-          backgroundPosition: 'center', 
-          height: '100vh' }}>
-            <div className="text-center hero-content" style={{ paddingTop: '60vh' }}>
-              <h2 className="text-2xl md:text-4xl font-semibold mb-10">Find your unique football shirts</h2>
-              <div>
-                <Link to={"/all-shirts"}>
-                  <button
-                    style={{
-                      backgroundColor: '#007bff',
-                      color: '#fff',
-                      padding: '10px 20px',
-                      borderRadius: '5px',
-                      transition: 'background-color 0.3s, color 0.3s',
-                    }}
-                    onMouseEnter={(e: React.MouseEvent<HTMLButtonElement>) => {
-                      e.currentTarget.style.backgroundColor = '#fff';
-                      e.currentTarget.style.color = '#007bff';
-                    }}
-                    onMouseLeave={(e: React.MouseEvent<HTMLButtonElement>) => {
-                      e.currentTarget.style.backgroundColor = '#007bff';
-                      e.currentTarget.style.color = '#fff';
-                    }}
-                  >
-                    Discover
-                  </button>
-
-                </Link>
-              </div>
-
-            </div>
-          </div>
-          <div className="recently-added">
-            <h2>Recently Added</h2>
-            <RecentlyAddedShirts shirts={shirts} />
+      <div id="root" className="bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${netherlandsHomeKitImage})` }}>
+        <div className="flex flex-col justify-center items-center h-full text-white">
+          <h2 className="text-2xl md:text-4xl mb-4">Find your unique football shirts</h2>
+          <div>
+            <Link to="/all-shirts">
+              <button className="bg-blue-500 text-white py-2 px-4 rounded transition duration-300 hover:bg-white hover:text-blue-500">
+                Discover
+              </button>
+            </Link>
           </div>
         </div>
-      </>
+        <div className="recently-added">
+          <h2 className="text-2xl">Recently Added</h2>
+          <RecentlyAddedShirts shirts={shirts} />
+        </div>
+      </div>
     );
-  }
+  };
   return (
     <>
       <div id="root">
