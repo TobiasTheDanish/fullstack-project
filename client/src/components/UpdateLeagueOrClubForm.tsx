a7f6ede0ac2f94410aec282f648e178d5dc82e39
TobiasTheDanish <t.hc199811@gmail.com>
2024-03-17 23:04:34 +0100

    add component to update a league or a club

--- client/src/components/UpdateLeagueOrClubForm.tsx
+++ client/src/components/UpdateLeagueOrClubForm.tsx
@@ -1,35 +1,84 @@
 import { useEffect, useRef, useState } from "react"
 import { ClubSelect, ImageUpload } from ".";
 import { LeagueSelect } from "./leagues/LeagueSelect";
+import { Club, League } from "../graphql/types";
+import { useMutation } from "@apollo/client";
+import { MutationUpdateLeague, gqlUpdateLeagueMutation } from "../graphql/league";
+import { gqlUpdateClub } from "../graphql/club";
 
 export function UpdateLeagueOrClubForm() {
   const typeSelectRef = useRef<HTMLSelectElement>(null);
-  const selectorRef = useRef<HTMLSelectElement>(null);
+  const [selectedLeague, setSelectedLeague] = useState<Partial<Omit<League, 'clubs' | 'createdAt'>>>({});
+  const [selectedClub, setSelectedClub] = useState<Partial<Omit<Club, 'league' | 'createdAt' |  'shirts'>>>({});
   const [selectedType, setSelectedType] = useState("");
   const [image, setImage] = useState("");
 
-  useEffect(() => {
-    const selected = typeSelectRef.current?.value! == "0" ? "League" : "Club";
+  const [updateLeague, {data: leagueData, error: leagueError}] = useMutation<MutationUpdateLeague>(gqlUpdateLeagueMutation)
+  const [updateClub, {data: clubData, error: clubError}] = useMutation(gqlUpdateClub);
 
-    setSelectedType(selected);
+  useEffect(() => {
     
   }, [typeSelectRef.current?.value]);
 
-  const onUpdateHandler = () => {
+  const onUpdateHandler = async () => {
+    if (selectedType == "League") {
+      const updateInput = {
+        name: selectedLeague.name!,
+        country: selectedLeague.country!,
+        imageUrl: image,
+      }
 
+      await updateLeague({
+        variables: {
+          leagueId: selectedLeague._id,
+          input: updateInput,
+        }
+      });
+
+      if (leagueData) {
+        console.log("league updated");
+        console.log(leagueData);
+      } else {
+        console.log("league not updated");
+        console.log(leagueError);
+      }
+    } else {
+      const updateInput = {
+        name: selectedClub.name!,
+        imageUrl: image,
+      }
+
+      await updateClub({
+        variables: {
+          clubId: selectedClub._id!,
+          input: updateInput,
+        }
+      });
+      if (clubData) {
+        console.log("league updated");
+        console.log(clubData);
+      } else {
+        console.log("league not updated");
+        console.log(clubError);
+      }
+    }
   };
   
   return (
     <>
-      <select ref={typeSelectRef}>
+      <select ref={typeSelectRef} onChange={() => {
+        const selected = typeSelectRef.current?.value! == "0" ? "League" : "Club";
+
+        setSelectedType(selected);
+      }}>
         <option>Select update type</option>
         <option value="0">League</option>
         <option value="1">Club</option>
       </select>
       { selectedType == "League" ? (
-          <LeagueSelect ref={selectorRef} />
+          <LeagueSelect setLeague={setSelectedLeague} />
         ) : (
-          <ClubSelect ref={selectorRef} />
+          <ClubSelect setClub={setSelectedClub} />
         )
       }
       {image == "" ? <ImageUpload setImage={setImage}/> :
