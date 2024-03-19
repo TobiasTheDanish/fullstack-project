311415bd277d8133da2f1a3fcebda49e1176ba99
ChristianStennicke <cph-cs519@cphbusiness.dk>
2024-03-19 13:13:41 +0100

    JWT fix

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -8,8 +8,11 @@ function setJWT(token:string): void {
 
 function getJWT(): string {
 	const token = window.localStorage.getItem("JWT");
-
-	return token ?? "";
+	if(!token){
+		console.log("jwt not found localStorage")
+		return ""
+	}
+	return token;
 }
 
 function isLoggedIn(): boolean {
